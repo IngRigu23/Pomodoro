d3c3eebf22e8f55fc7623b4d5cdee697c6721533
Ricardo Guerrero <ricardo.guerreros@hotmail.com>
2024-08-30 16:43:02 -0600

    add timer mechanisms

--- main.py
+++ main.py
@@ -14,11 +14,41 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    #label = Label(text="Timer", font=(FONT_NAME, 34, "bold"), fg=GREEN, bg=YELLOW)
+    #label.grid(column=1,row=0)
+    label_check.grid(column=1, row=3)
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_time():
-    count_down(WORK_MIN*60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+    #attempt
+    #attempt = reps % 2
+    #if its the 1st/3rd/5th/7th reps:
+    if reps % 8 == 0:
+        label = Label(text="Break", font=(FONT_NAME, 34, "bold"), fg=RED, bg=YELLOW)
+        label.grid(column=1, row=0)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        label = Label(text="Break", font=(FONT_NAME, 34, "bold"), fg=PINK, bg=YELLOW)
+        label.grid(column=1, row=0)
+        count_down(short_break_sec)
+    else:
+        label = Label(text="Work time", font=(FONT_NAME, 34, "bold"), fg=GREEN, bg=YELLOW)
+        label.grid(column=1,row=0)
+        count_down(work_sec)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count/60)
@@ -27,7 +57,15 @@ def count_down(count):
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count> 0:
-        window.after(1000,count_down,count-1)
+        global timer
+        timer = window.after(1000,count_down,count-1)
+    else:
+        start_time()
+        mark = ""
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            mark += "✅"
+        label_check.config(text="mark")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -40,19 +78,15 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=tomato_img)
 timer_text = canvas.create_text(100,130, text= "00:00", fill="white", font=(FONT_NAME,34, "bold"))
 label = Label( text= "Timer", font=(FONT_NAME,34, "bold"), fg=GREEN, bg= YELLOW)
-label_check = Label( text= "✔", font=(FONT_NAME,24, "bold"), fg=GREEN, bg= YELLOW)
+label_check = Label(font=(FONT_NAME,24, "bold"), fg=GREEN, bg= YELLOW)
 
 button_start = Button(text="Start", command=start_time, highlightthickness=0)
 button_start.grid(column=0,row=3)
 
-
-
-button_reset = Button(text="Reset", highlightthickness=0)
+button_reset = Button(text="Reset", highlightthickness=0, command=reset)
 button_reset.grid(column=3,row=3)
 label.grid(column=1,row=0)
 canvas.grid(column=1,row=2)
 label_check.grid(column=1,row=3)
-
-
 
 window.mainloop()
