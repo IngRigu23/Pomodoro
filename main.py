e5b7fd6ebe017846205c0d1cf711fd2e4fb200c4
Ricardo Guerrero <ricardo.guerreros@hotmail.com>
2024-08-30 14:01:10 -0600

    add countdown mechanisms

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 from numpy.ma.core import count
 from pandas.core.window import Window
@@ -16,10 +17,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_time():
+    count_down(WORK_MIN*60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count> 0:
         window.after(1000,count_down,count-1)
 
@@ -35,9 +40,12 @@ canvas.create_image(100,112,image=tomato_img)
 timer_text = canvas.create_text(100,130, text= "00:00", fill="white", font=(FONT_NAME,34, "bold"))
 label = Label( text= "Timer", font=(FONT_NAME,34, "bold"), fg=GREEN, bg= YELLOW)
 label_check = Label( text= "✔", font=(FONT_NAME,24, "bold"), fg=GREEN, bg= YELLOW)
-count_down(5)
-button_start = Button(text="Start", highlightthickness=0)
+
+button_start = Button(text="Start", command=start_time, highlightthickness=0)
 button_start.grid(column=0,row=3)
+
+
+
 button_reset = Button(text="Reset", highlightthickness=0)
 button_reset.grid(column=3,row=3)
 label.grid(column=1,row=0)
